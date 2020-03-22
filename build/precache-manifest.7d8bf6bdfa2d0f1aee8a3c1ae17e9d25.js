1fa23da242e739c7d1d51900bce1a33184487e47
jakub_banasiak <soccerkid.bany@gmail.com>
2020-03-22 11:45:59 +0100

    add build to github

---
+++ build/precache-manifest.7d8bf6bdfa2d0f1aee8a3c1ae17e9d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0201d4cce4f2e0445c98aa826accde5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "411cf640b28ae0a58807",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3839d3598433e07aa4a2",
+    "url": "/static/js/2.55093879.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55093879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411cf640b28ae0a58807",
+    "url": "/static/js/main.20436235.chunk.js"
+  },
+  {
+    "revision": "6128176043686e13f03c",
+    "url": "/static/js/runtime-main.a3f7a3f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
