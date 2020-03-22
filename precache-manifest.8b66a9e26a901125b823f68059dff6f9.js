64f05c764ba1aea5c56db45895a5129ebfb58498
jakub_banasiak <soccerkid.bany@gmail.com>
2020-03-22 11:59:53 +0100

    Updates

---
+++ precache-manifest.8b66a9e26a901125b823f68059dff6f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769d59252b54c7b1ff9fd4c11b66a80f",
+    "url": "/6c-in-react/index.html"
+  },
+  {
+    "revision": "5a0e2b46fd3904b444f3",
+    "url": "/6c-in-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74bed143b11726d4bdaa",
+    "url": "/6c-in-react/static/js/2.3e8688a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/6c-in-react/static/js/2.3e8688a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0e2b46fd3904b444f3",
+    "url": "/6c-in-react/static/js/main.3a7cf3bd.chunk.js"
+  },
+  {
+    "revision": "8aefc91b71c747d2b656",
+    "url": "/6c-in-react/static/js/runtime-main.10e89951.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/6c-in-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
